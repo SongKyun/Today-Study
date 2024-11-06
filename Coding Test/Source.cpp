e75e7edc8e98fd4587f15a3935226b363f39c752
SongKyun <89376275+SongKyun@users.noreply.github.com>
2024-11-06 23:13:08 +0900

    11/6
    bool compare

--- Coding Test/Source.cpp
+++ Coding Test/Source.cpp
@@ -15,3 +15,17 @@ vector<int> solution(vector<int> numlist, int n) {
 		});
 	return numlist;
 }
+
+int num;
+
+bool compare(int prev, int next) {
+	if (abs(prev - num) == abs(next - num))
+		return prev > next;
+	return abs(prev - num) < abs(next - num);
+}
+
+vector<int> solution(vector<int> numlist, int n) {
+	num = n;
+	sort(numlist.begin(), numlist.end(), compare);
+	return numlist;
+}
