b1d09d3918ccb1a3b36e922e60d2efa2c600e4a3
SongKyun <89376275+SongKyun@users.noreply.github.com>
2024-11-07 23:43:01 +0900

    11/7
    1062 가르침 풀이
    비트마스킹

--- Coding Test/Source.cpp
+++ Coding Test/Source.cpp
@@ -1,31 +1,49 @@
-#include<string>
-#include<vector>
-#include<algorithm>
-
+#include <iostream>
 using namespace std;
 
-vector<int> solution(vector<int> numlist, int n) {
-	sort(numlist.begin(), numlist.end(), [n](int a, int b) {
-		int distA = abs(a - n);
-		int distB = abs(b - n);
-		if (distA == distB) {
-			return a > b;
-		}
-		return distA < distB;
-		});
-	return numlist;
+int n, m, words[51];
+string s;
+
+int count(int mask) {
+
+    int cnt = 0;
+
+    for (int word : words) {
+
+        if (word && (word & mask) == word)
+            cnt++;
+    }
+    return cnt;
 }
 
-int num;
+int go(int index, int k, int mask) {
 
-bool compare(int prev, int next) {
-	if (abs(prev - num) == abs(next - num))
-		return prev > next;
-	return abs(prev - num) < abs(next - num);
+    if (k < 0) return 0;
+
+    if (index == 26) return count(mask);
+
+    int ret = go(index + 1, k - 1, mask | (1 << index));
+
+    if (index != 'a' - 'a' && index != 'n' - 'a' && index != 't' - 'a'
+        && index != 'i' - 'a' && index != 'c' - 'a') {
+        ret = max(ret, go(index + 1, k, mask));
+    }
+    return ret;
 }
 
-vector<int> solution(vector<int> numlist, int n) {
-	num = n;
-	sort(numlist.begin(), numlist.end(), compare);
-	return numlist;
+int main() {
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++) {
+
+        cin >> s;
+
+        for (char str : s) {
+
+            words[i] |= (1 << (str - 'a'));
+        }
+    }
+
+    cout << go(0, m, 0) << '\n';
+    return 0;
 }
