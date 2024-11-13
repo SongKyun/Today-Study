1c31dad7b0b2a09a529d5159e17ec4454df821d2
SongKyun <89376275+SongKyun@users.noreply.github.com>
2024-11-14 00:12:22 +0900

    11/14
    이분탐색 큰돌의 비밀 수첩

--- Coding Test/Source.cpp
+++ Coding Test/Source.cpp
@@ -1,33 +1,38 @@
-int binarySearch(const vector<int>& arr, int target) {
-	int left = 0;
-	int right = arr.size() - 1; // 가장 오른쪽
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
 
-	while (left <= right) {
-		int mid = left + (rigth - left) / 2; // 중앙 값
-		cout << mid << " : " << arr[mid] << '\n';
-		if (arr[mid] == target) {
-			return mid; // 타겟 값을 찾았을 때
-		}
-		else if (arr[mid] < target) {
-			left = mid + 1; // 타겟이 중간 값보다 크므로 오른쪽 부분 탐색
-		}
-		else {
-			right = mid - 1; // 타겟이 중간 값 보다 작으므로 왼쪽 부분을 탐색
-		}
+int t, n, m, temp;
+
+string check(int temp, vector<int> &v) {
+	int l = 0;
+	int r = v.size() - 1;
+	int mid;
+	while (l <= r) {
+		mid = l + (r - 1) / 2;
+		if (v[mid] > temp) r = mid - 1;
+		else if (v[mid] == temp) return "큰돌이는 기억해!";
+		else l = mid + 1;
 	}
-	return -1; // 값을 찾지 못했을 경우
+	return "큰돌이는 까먹었어!";
 }
 
 int main() {
-	vector<int> arr = { 1, 3, 6, 9, 10, 21, 22, 30 };
-	int target = 6;
-	int ret = binarySearch(arr, target);
-
-	if (result != -1) {
-		cout << " Target found at index : " << result << "\n";
-	}
-	else {
-		cout << "Target not found in the array \n";
+	cin >> t;
+	while (t--) {
+		vector<int> v;
+		cin >> n;
+		for (int i = 0; i < n; i++) {
+			cin >> temp;
+			v.push_back(temp);
+		}
+		sort(v.begin(), v.end());
+		cin >> m;
+		for (int i = 0; i < m; i++) {
+			cin >> temp;
+			cout << check(temp, v) << "\n";
+		}
 	}
 	return 0;
 }
