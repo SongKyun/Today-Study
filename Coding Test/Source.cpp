8f159fa1e676c5a9b9e38d21653b9901f457da6a
SongKyun <89376275+SongKyun@users.noreply.github.com>
2024-11-13 23:43:09 +0900

    11/13
    이분탐색

--- Coding Test/Source.cpp
+++ Coding Test/Source.cpp
@@ -1,46 +1,33 @@
-#include <iostream>
-using namespace std;
+int binarySearch(const vector<int>& arr, int target) {
+	int left = 0;
+	int right = arr.size() - 1; // 가장 오른쪽
 
-int n, m, words[51];
-string s;
-
-int count(int mask) {
-
-	int cnt = 0;
-
-	for (int word : words) {
-		if (word && (word & mask) == word)
-			cnt++;
-	}
-	return cnt;
-}
-
-int go(int index, int k, int mask) {
-
-	if (k < 0) return 0;
-	if (index == 26) return count(mask);
-
-	int ret = go(index + 1, k - 1, mask | (1 << index));
-
-	if (index != 'a' - 'a' && index != 'n' - 'a' && index != 't' - 'a'
-		&& index != 'i' - 'a' && index != 'c' - 'a') {
-		ret = max(ret, go(index + 1, k, mask));
-	}
-	return ret;
-}
-
-main() {
-	
-	cin >> n >> m;
-
-	for (int i = 0; i < n; i++) {
-		
-		cin >> s;
-
-		for (char str : s) {
-			words[i] |= (1 << (str - 'a'));
+	while (left <= right) {
+		int mid = left + (rigth - left) / 2; // 중앙 값
+		cout << mid << " : " << arr[mid] << '\n';
+		if (arr[mid] == target) {
+			return mid; // 타겟 값을 찾았을 때
+		}
+		else if (arr[mid] < target) {
+			left = mid + 1; // 타겟이 중간 값보다 크므로 오른쪽 부분 탐색
+		}
+		else {
+			right = mid - 1; // 타겟이 중간 값 보다 작으므로 왼쪽 부분을 탐색
 		}
 	}
-	cout << go(0, m, 0) << '\n';
+	return -1; // 값을 찾지 못했을 경우
+}
+
+int main() {
+	vector<int> arr = { 1, 3, 6, 9, 10, 21, 22, 30 };
+	int target = 6;
+	int ret = binarySearch(arr, target);
+
+	if (result != -1) {
+		cout << " Target found at index : " << result << "\n";
+	}
+	else {
+		cout << "Target not found in the array \n";
+	}
 	return 0;
 }
