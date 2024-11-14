2fb3024026d94a8df9276c36475de7e73cfa7e37
SongKyun <89376275+SongKyun@users.noreply.github.com>
2024-11-14 22:45:40 +0900

    11/14
    큰돌 형광펜 질투쟁이 문제

--- Coding Test/Source.cpp
+++ Coding Test/Source.cpp
@@ -1,38 +1,48 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 using namespace std;
 
-int t, n, m, temp;
+int n, m, result = 987654321; // 친구, 색상, 최소 질투심 초기값
+int arr[300000]; // 각 색상의 형광펜 개수를 저장하는 배열
 
-string check(int temp, vector<int> &v) {
-	int l = 0;
-	int r = v.size() - 1;
-	int mid;
-	while (l <= r) {
-		mid = l + (r - 1) / 2;
-		if (v[mid] > temp) r = mid - 1;
-		else if (v[mid] == temp) return "큰돌이는 기억해!";
-		else l = mid + 1;
+// 특정 질투심으로 형광펜을 분배할 수 있는지 확인하는 함수
+bool search(int estimate_val)
+{
+	int ret = 0; // 필요한 친구 수
+	for (int i = 0; i < m; i++)
+	{
+		ret += arr[i] / estimate_val; // 나눈 몫을 추가함
+		if (arr[i] % estimate_val) ret++; // 나머지가 있다면 1명이 더 필요
 	}
-	return "큰돌이는 까먹었어!";
+	return ret <= n; // n 이하인 경우 True 반환
 }
 
-int main() {
-	cin >> t;
-	while (t--) {
-		vector<int> v;
-		cin >> n;
-		for (int i = 0; i < n; i++) {
-			cin >> temp;
-			v.push_back(temp);
+int main()
+{
+	cin >> n >> m;
+
+	int left = 1, right = 0, mid;
+
+	for (int i = 0; i < m; ++i)
+	{
+		cin >> arr[i]; // 각 색상의 형광펜 개수 입력
+		right = max(right, arr[i]);
+	}
+
+	// 이진 탐색을 통해 최소 질투심 구하기
+	while (left <= right)
+	{
+		mid = (left + right) / 2; // 중간값
+
+		if (search(mid)) // mid 값으로 분배가 가능한 경우
+		{
+			result = min(result, mid); // 최소 질투심 갱신
+			right = mid - 1; // 더 작은 질투심 탐색
 		}
-		sort(v.begin(), v.end());
-		cin >> m;
-		for (int i = 0; i < m; i++) {
-			cin >> temp;
-			cout << check(temp, v) << "\n";
+		else
+		{
+			left = mid + 1; // 더 큰 질투심 탐색
 		}
 	}
+	cout << result << '\n';
 	return 0;
 }
