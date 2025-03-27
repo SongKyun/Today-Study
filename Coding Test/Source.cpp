bd3a743cc9e1276c604b671df71a22cbfe715cd0
SongKyun <89376275+SongKyun@users.noreply.github.com>
2025-03-27 18:37:33 +0900

    25/3/27
    복습 BFS DFS 추가 이해

--- Coding Test/Source.cpp
+++ Coding Test/Source.cpp
@@ -1,89 +1,4 @@
-﻿//#include <iostream>
-//
-//using namespace std;
-//
-//struct Node
-//{
-//	char data;
-//	Node* left;
-//	Node* right;
-//
-//	Node(char parent)
-//	{
-//		data = parent;
-//		left = right = nullptr;
-//	}
-//};
-//
-//void insert(Node*& root, char parent, char left, char right) {
-//	if (root == nullptr) root = new Node(parent);
-//
-//	if (root->data == parent) {
-//		if (left != '.') root->left = new Node(left);
-//		if (right != '.') root->right = new Node(right);
-//	}
-//	else {
-//		if (root->left) insert(root->left, parent, left, right);
-//		if (root->right) insert(root->right, parent, left, right);
-//	}
-//}
-//
-//void preorder(Node* root)
-//{
-//	if (root)
-//	{
-//		cout << root->data;
-//		preorder(root->left);
-//		preorder(root->right);
-//	}
-//}
-//
-//void inorder(Node* root)
-//{
-//	if (root)
-//	{
-//		inorder(root->left);
-//		cout << root->data;
-//		inorder(root->right);
-//	}
-//}
-//
-//void postorder(Node* root)
-//{
-//	if (root)
-//	{
-//		postorder(root->left);
-//		postorder(root->right);
-//		cout << root->data;
-//	}
-//}
-//
-//int main()
-//{
-//	Node* root = nullptr;
-//
-//	int N;
-//	cin >> N;
-//
-//	char parent, left, right;
-//
-//	for (int i = 0; i < N; i++) {
-//		cin >> parent >> left >> right;
-//		insert(root, parent, left, right);
-//	}
-//
-//	preorder(root);
-//	cout << endl;
-//	inorder(root);
-//	cout << endl;
-//	postorder(root);
-//
-//	return 0;
-//}
-
-// 트리의 부모 찾기
-
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 #include <queue>
 using namespace std;
@@ -93,29 +8,33 @@ vector<int> tree[100001]; // 트리(인접 리스트)
 int parent[100001]; // 부모 노드 저장 배열
 
 // BFS
-void bfs(int root) {
-	queue<int> q;
-	q.push(root);
+void bfs(int root) { // 초기 시작점(root) 하나를 알면돼서 매개변수가 root
+	queue<int> q; // 큐 선언 : 같은 깊이의 노드를 먼저 방문해야해서
+	//방문해야 할 노드들을 저장하고 순서대로 처리하기 위해
+	q.push(root); // 루트 노드 큐에 추가
 
 	while (!q.empty()) {
-		int cur = q.front();
-		q.pop();
+		int cur = q.front(); // cur 현재 방문할 노드 = 큐의 맨 앞 노드를 가져옴
+		q.pop(); // 큐에서 제거
 
+		// visited 방문 체크 역할을 트리는 사이클이 없어 같은 노드를 두 번 방문하지 않기에
+		// 부모 설정하는 배열(parent[])만으로 방문 체크 가능하다.
+		// 현재 노드(cur)의 자식 노드들(next)를 탐색
 		for (int next : tree[cur]) {
 			if (parent[next] == 0) { // next에 없다면(방문 안 한 노드라면)
-				parent[next] = cur; // 부모 설정
-				q.push(next);
+				parent[next] = cur; // 현재 노드를 부모 설정
+				q.push(next); // 다음 방문할 노드를 큐에 추가
 			}
 		}
 	}
 }
 
 // DFS
-void dfs(int cur) {
+void dfs(int cur) { // 매번 현재 노드(cur)를 매개변수로 전달
 	for (int next : tree[cur]) {
-		if (parent[next] == 0) {
-			parent[next] = cur;
-			dfs(next);
+		if (parent[next] == 0) { // 아직 방문 안 했다면
+			parent[next] = cur; // 현재 노드를 부모 설정
+			dfs(next); // 재귀 호출을 통해 깊이 우선 탐색 진행
 		}
 	}
 }
@@ -133,7 +52,7 @@ int main()
 	}
 
 	parent[1] = -1; // 루트 노드(1)의 부모는 없음
-	bfs(1); // BFS 탐색 시작
+	bfs(1); // BFS 탐색 시작(1 = int root 매개변수)
 
 	for (int i = 2; i <= N; i++) {
 		cout << parent[i] << '\n';
