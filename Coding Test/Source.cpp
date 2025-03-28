ba73822dd2e4903c3707d3cb6b1873161f87687b
SongKyun <89376275+SongKyun@users.noreply.github.com>
2025-03-28 21:24:04 +0900

    25/3/28
    복습

--- Coding Test/Source.cpp
+++ Coding Test/Source.cpp
@@ -1,37 +1,34 @@
-﻿// 9934 완전 이진 트리
-#include <iostream>
-#include <vector>
+﻿#include<iostream>
+#include<vector>
 using namespace std;
 
-int K;						// 트리 깊이
-vector<int> inorder;		// 중위 순회 결과 저장
-vector<vector<int>> levels; // 각 레벨의 노드 저장
+int K;
+vector<int> inorder;
+vector<vector<int>> levels;
 
-// 트리 재구성 함수
 void buildTree(int start, int end, int depth) {
-	if (start > end) return;				// 범위 벗어나면 종료
+	if (start > end) return;
 
-	int mid = (start + end) / 2;			// 중간값이 현재 서브트리의 루트
-	levels[depth].push_back(inorder[mid]);	// 해당 레벨에 노드 추가
+	int mid = (start + end) / 2;
+	levels[depth].push_back(inorder[mid]);
 
-	buildTree(start, mid - 1, depth + 1);	// <왼쪽 부분> 서브트리 재귀 처리
-	buildTree(mid + 1, end, depth + 1);		// <오른쪽 부분> 서브트리 재귀 처리
+	buildTree(start, mid - 1, depth + 1);
+	buildTree(mid + 1, end, depth + 1);
 }
 
 int main() {
-
 	cin >> K;
-	int numNodes = (1 << K) - 1;			// 2^k- 1 개의 노드
+	int numNodes = (1 << K) - 1;
 	inorder.resize(numNodes);
 	levels.resize(K);
 
-	for (int i = 0; i < numNodes; i++) {	// 중위 순회 결과 입력 받기
+	for (int i = 0; i < numNodes; i++) {
 		cin >> inorder[i];
 	}
 
-	buildTree(0, numNodes - 1, 0);			// 트리 구축
+	buildTree(0, numNodes - 1, 0);
 
-	for (int i = 0; i < K; i++) {			// 출력
+	for (int i = 0; i < K; i++) {
 		for (int val : levels[i]) {
 			cout << val << " ";
 		}
